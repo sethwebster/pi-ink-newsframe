b24ef431049eae402f57435c4ad60ce4aad9903d
Seth Webster <sethwebster@fb.com>
2020-07-30 19:34:43 -0400

    Adds install step to allow PDF rendering

--- install.sh
+++ install.sh
@@ -4,5 +4,7 @@ echo "Enabling spi..."
 sudo sed -i 's/#dtparam=spi=on/dtparam=spi=on/g' /boot/config.txt
 echo "Installing ImageMagick"
 sudo apt-get install imagemagick -y
+sudo sed -i 's/<policy domain="coder" rights="none" pattern="PDF" \/>/<policy domain="coder" rights="read|write" pattern="PDF" \/>/g' /etc/ImageMagick-6/policy.xml
+
 pip3 install spidev
 sudo shutdown -r now
