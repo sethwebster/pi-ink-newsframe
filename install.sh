8e63f2006714632b2cce7f44a3439aaefc6d8a31
Seth Webster <sethwebster@fb.com>
2020-07-30 19:37:06 -0400

    adds some output text

--- install.sh
+++ install.sh
@@ -4,7 +4,11 @@ echo "Enabling spi..."
 sudo sed -i 's/#dtparam=spi=on/dtparam=spi=on/g' /boot/config.txt
 echo "Installing ImageMagick"
 sudo apt-get install imagemagick -y
+echo "Enabling PDF rendering"
 sudo sed -i 's/<policy domain="coder" rights="none" pattern="PDF" \/>/<policy domain="coder" rights="read|write" pattern="PDF" \/>/g' /etc/ImageMagick-6/policy.xml
-
+echo "Installing spidev..."
 pip3 install spidev
+echo "Reboot required..."
+echo "Wait 5 seconds for reboot, or ctrl-c to cancel"
+sleep 5
 sudo shutdown -r now
