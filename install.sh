51d154ff6f74f5f9d353fa53902023bf84da5038
Seth Webster <sethwebster@fb.com>
2020-07-30 19:15:56 -0400

    Adds install script

---
+++ install.sh
@@ -0,0 +1,7 @@
+echo "Updating system..."
+sudo apt-get update && sudo apt-get upgrade
+echo "Enabling spi..."
+sudo sed -i 's/#dtparam=spi=on/dtparam=spi=on/g' /boot/config.txt
+echo "Installing ImageMagick"
+sudo apt-get install imagemagick -y
+pip3 install spidev
