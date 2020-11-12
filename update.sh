215d453e9ec82f593064b8f5e7359b528d79e09b
Seth Webster <sethwebster@gmail.com>
2020-11-12 14:59:12 -0500

    depth=1

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ sudo kill $ID
 ID=$(ps aux | grep python | grep server.py | awk '{ print $2 }')
 sudo kill $ID
 
-cd /home/pi/pi-ink-newsframe && git pull origin master -f && chown pi.pi -R *
+cd /home/pi/pi-ink-newsframe && git pull origin master -f --depth=1 && chown pi.pi -R *
 
 # python3 /home/pi/pi-ink-newsframe/main.py & 
 # python3 /home/pi/pi-ink-newsframe/server.py & 
